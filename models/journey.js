9326371dbeaa259ad5bf72266aad8b600ea22911
Maxime Violi <maxime.violi@gmail.com>
2021-07-29 14:24:00 +0200

    fabrication homepage

---
+++ models/journey.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var journeySchema = mongoose.Schema({
+    departure: String,
+    arrival: String,
+    date: Date,
+    departureTime: String,
+    price: Number,
+  });
+  
+  var JourneyModel = mongoose.model('journeys', journeySchema);
+
+  module.exports = JourneyModel;
+
+  
+  var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
+  var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
