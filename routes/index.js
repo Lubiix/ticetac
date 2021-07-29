f2511cd04e3d7556dc208643cd7e7a0dad09fde0
Geoffroy Farrugia <g.farrugia@outlook.com>
2021-07-29 14:23:49 +0200

    sign-in first commit

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,11 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 
-
-// --------------------- BDD -----------------------------------------------------
-
-
-var journeySchema = mongoose.Schema({
-  departure: String,
-  arrival: String,
-  date: Date,
-  departureTime: String,
-  price: Number,
-});
-
-var journeyModel = mongoose.model('journey', journeySchema);
-
-var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
-var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
-
-
+var JourneyModel = require ('../models/journey')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('signin', { title: 'Express' });
 });
 
 
